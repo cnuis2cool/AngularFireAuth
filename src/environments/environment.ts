868b62137e06c7e753b3438625f0882a28b49089
Srinivas Panuganti <spanugan@gmail.com>
2021-01-04 22:00:03 +0530

    Angular app with Firebase Auth

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxx"
+  }
+};
+
